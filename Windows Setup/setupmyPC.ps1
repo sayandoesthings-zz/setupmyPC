0614138aa4c4fdff025ec505d8f1e8bf89411466
Sayan Banerjee <61379891+sayandoesthings@users.noreply.github.com>
2020-06-13 23:13:51 +0530

    changed the ninite install, changed the ps1 script

--- Windows Setup/setupmyPC.ps1
+++ Windows Setup/setupmyPC.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
- choco install -y teracopy vscode nodejs git github-desktop
+ choco install -y powertoys nodejs github-desktop
